7b8c169d01c830ca144f8d3d09c4abb1cadf86ef
Kinh <hagiakinh0209@gmail.com>
2024-06-02 13:44:15 +0700

    using openai for chatbot instead of openrouter to avoid jetson's dependency conflict

--- SmartMirrorCapstoneProject2024/lib/LlmChatBot/LlmChatBot.py
+++ SmartMirrorCapstoneProject2024/lib/LlmChatBot/LlmChatBot.py
@@ -1,11 +1,11 @@
-from llama_index.llms.openrouter import OpenRouter
-llm = OpenRouter(
-    api_key="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",
-    max_tokens=256,
-    context_window=4096,
-    model="google/gemma-7b-it:free",
-)
+from openai import OpenAI
+from os import getenv
 
+# gets API Key from environment variable OPENAI_API_KEY
+client = OpenAI(
+  base_url="https://openrouter.ai/api/v1",
+  api_key="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",
+)
 
 class AskChatBot:
     def __init__(self) -> None:
@@ -13,5 +13,20 @@ class AskChatBot:
     def setNotifier(self, notifier):
         self.notifier = notifier
     def ask(self, msg):
-        self.response = llm.complete(msg)
-        self.notifier(str(self.response))
+        completion = client.chat.completions.create(
+        model="google/gemma-7b-it:free",
+        messages=[
+            {
+            "role": "user",
+            "content": ""+ str(msg),
+            },
+        ],
+        )
+        self.notifier(str(completion.choices[0].message.content))
+# if __name__== "__main__":
+#     a = AskChatBot()
+#     def b(msg):
+#         print(msg)
+#     a.setNotifier(b)
+#     a.ask(" how many legs does an ant have")
+
